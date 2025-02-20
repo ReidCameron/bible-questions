a1d93e739bee25704d450887de4b4daf06517cfe
Cameron Reid <cameron.reid@searchspring.com>
2025-02-20 17:53:07 -0600

    add message endpoint

--- server/routers/apiRouter.js
+++ server/routers/apiRouter.js
@@ -6,6 +6,10 @@ router.get('/', async (req, res, next) =>{
     res.json({'message': 'Welcome to the demo API.'});
 });
 
+router.post('/message', async (req, res, next) =>{
+    console.log("[BQ] POST BODY:", req.body.toString())
+});
+
 //404 - Endpoint does not exist
 router.use((req, res) => {
     res.status(404).json({'message': 'Endpoint does not exist.'});
