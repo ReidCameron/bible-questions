7dd737e6c0a1032f7048910bbc0f16bca0db964a
Cameron Reid <cameron.reid@searchspring.com>
2025-02-26 23:43:58 -0600

    add function

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@
 const express = require('express');
 const serverless = require('serverless-http');
 const app = express();
-const { connectLambda } = require("@netlify/blobs");
+const { connectLambda, getStore } = require("@netlify/blobs");
 
 // Express Config
 app.engine('ejs', require('ejs').__express);
